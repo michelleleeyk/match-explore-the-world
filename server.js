d7548874982f5c324a1ad96c9e22a753e97beb4b
michelleleeyk <70876461+michelleleeyk@users.noreply.github.com>
2021-07-09 18:46:41 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import Datastore from "nedb";
+
+// let the Port number be whatever Heroku gives us, or 3000 if running app locally.
+const port = process.env.PORT || 3000;
+const app = express();
+
+const leaderboard = new Datastore("scoreList.db");
+leaderboard.loadDatabase();
+
+app.use(express.static("static"));
+app.use(express.json());
+
+//Submit name & score into leaderboard db.
+app.post("/scoreRoom", function (request, response) {
+    const playerInfo = request.body;
+    leaderboard.insert(playerInfo);
+    response.json(playerInfo);
+});
+
+//Show leaderboard
+app.get("/scoreRoom", function (request, response) {
+    leaderboard.find({}, function (err, playerInfo) {
+        if (err) {
+            response.end();
+            //response.send({ error: "Internal Server Error" });
+            return;
+        }
+        response.json(playerInfo);
+    });
+});
+app.listen(port, function () {
+    console.log("listening at port" + port);
+});
